048b8142a781bccd6d85d49993dba13deda15c61
Carol <carol@CarolnoMacBook-Air.local>
2021-06-08 19:14:46 -0300

    add folder public

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,3 @@
-import "@fontsource/open-sans"
-import "@fontsource/arimo"
-import "@fontsource/pacifico"
+import '@fontsource/open-sans';
+import '@fontsource/arimo';
+import '@fontsource/pacifico';
