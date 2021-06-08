d22aa6c611434f834cc30825c87b7af67ceea353
Carol <carol@CarolnoMacBook-Air.local>
2021-06-08 20:04:39 -0300

    create folder docs to github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: "Karamello Sorvetes",
-    description: "Karamello Sorvetes - Loja de fábrica, atacado e varejo do melhor sorvete, picolé e açaí da região de Suzano e Alto Tietê. Compre no açaí, sorvete e picolé no atacado para revenda",
-    url: "http://karamellosorvetes.com.br"
+    title: 'Karamello Sorvetes',
+    description: 'Karamello Sorvetes - Loja de fábrica, atacado e varejo do melhor sorvete, picolé e açaí da região de Suzano e Alto Tietê. Compre no açaí, sorvete e picolé no atacado para revenda',
+    url: 'http://karamellosorvetes.com.br',
   },
   plugins: [
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-eslint",
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-eslint',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
 };
-
 
 // {
 //   resolve: "gatsby-plugin-google-analytics",
